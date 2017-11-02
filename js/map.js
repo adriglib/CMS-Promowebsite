3f9eeedcf356742106d06860d54a014eaddc2dce
adriglib <adriglib@student.arteveldehs.be>
2017-11-02 20:21:47 +0100

    added leaflet and contact

--- js/map.js
+++ js/map.js
@@ -1,8 +1,12 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([51.061836, 3.712511], 12.45);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/adriglib/cj9ire1o635ak2qpar3gow3sy/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWRyaWdsaWIiLCJhIjoiY2l3bHVxYm1wMDAwMjJ0bnE2MWp3azhmdiJ9.E3Udm-vhUj4CEWJuCC_big', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiYWRyaWdsaWIiLCJhIjoiY2l3bHVxYm1wMDAwMjJ0bnE2MWp3azhmdiJ9.E3Udm-vhUj4CEWJuCC_big'
 }).addTo(mymap);
+
+var mariakerke = L.marker([51.087144, 3.6702904]).addTo(mymap);
+var gentSintPieters = L.marker([51.0361237, 3.7087479]).addTo(mymap);
+var korenmarkt = L.marker([51.054633,3.7197544]).addTo(mymap);
